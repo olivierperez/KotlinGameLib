732666630580ceb6dde126bb316391fcf661fe94
Olivier PEREZ <olivier@olivierperez.fr>
2023-02-12 22:30:07 +0100

    WIP Font Texture

--- Carres/src/nativeMain/kotlin/fr/o80/carres/image/TextureLoader.kt
+++ Carres/src/nativeMain/kotlin/fr/o80/carres/image/TextureLoader.kt
@@ -1,5 +1,6 @@
 package fr.o80.carres.image
 
+import fr.o80.gamelib.fontatlas.FontAtlas
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
@@ -8,43 +9,52 @@ import kotlinx.cinterop.value
 import platform.opengl32.*
 
 class TextureLoader {
-    fun load(image: Image): Texture {
-        val textureId = memScoped {
-            val texture = alloc<GLuintVar>()
-            glGenTextures(1, texture.ptr)
-
-            glBindTexture(GL_TEXTURE_2D, texture.value)
-
-            val pixels = image.colors
-                .map { (red, green, blue) ->
-                    blue.shl(16) + green.shl(8) + red
-                }
-                .toIntArray()
-
-            glTexImage2D(
-                GL_TEXTURE_2D,
-                0,
-                GL_RGB,
-                image.width,
-                image.height,
-                0,
-                GL_RGBA,
-                GL_UNSIGNED_BYTE,
-                pixels.refTo(0)
-            )
-
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-
-            glBindTexture(GL_TEXTURE_2D, 0)
-
-            texture.value
-        }
-
+    fun loadTexture(image: Image): Texture {
         return Texture(
             image.width,
             image.height,
-            textureId
+            createTexture(image)
         )
+    }
+
+    fun loadFontTexture(fontAtlas: FontAtlas, image: Image): FontTexture {
+        return FontTexture(
+            image.width,
+            image.height,
+            createTexture(image),
+            fontAtlas
+        )
+    }
+
+    private fun createTexture(image: Image) = memScoped {
+        val texture = alloc<GLuintVar>()
+        glGenTextures(1, texture.ptr)
+
+        glBindTexture(GL_TEXTURE_2D, texture.value)
+
+        val pixels = image.colors
+            .map { (red, green, blue) ->
+                blue.shl(16) + green.shl(8) + red
+            }
+            .toIntArray()
+
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RGB,
+            image.width,
+            image.height,
+            0,
+            GL_RGBA,
+            GL_UNSIGNED_BYTE,
+            pixels.refTo(0)
+        )
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+        glBindTexture(GL_TEXTURE_2D, 0)
+
+        texture.value
     }
 }
--- Carres/src/nativeMain/kotlin/fr/o80/carres/scenes/main/MainScene.kt
+++ Carres/src/nativeMain/kotlin/fr/o80/carres/scenes/main/MainScene.kt
@@ -2,10 +2,14 @@ package fr.o80.carres.scenes.main
 
 import fr.o80.carres.CarresSceneManager
 import fr.o80.carres.image.BmpReader
+import fr.o80.carres.image.FontTexture
 import fr.o80.carres.image.Texture
 import fr.o80.carres.image.TextureLoader
 import fr.o80.gamelib.Scene
 import fr.o80.gamelib.dsl.draw
+import fr.o80.gamelib.fontatlas.FileContentProvider
+import fr.o80.gamelib.fontatlas.FontAtlas
+import fr.o80.gamelib.fontatlas.FontAtlasLoader
 import fr.o80.gamelib.loop.KeyPipeline
 import fr.o80.gamelib.loop.MouseButtonPipeline
 import fr.o80.gamelib.loop.MouseMovePipeline
@@ -18,6 +22,10 @@ import okio.Path.Companion.toPath
 class MainScene(
     private val sceneManager: CarresSceneManager
 ) : Scene {
+
+    private val fontAtlasLoader = FontAtlasLoader()
+    private lateinit var fontAtlas: FontAtlas
+    private lateinit var fontTexture: FontTexture
 
     private var texture: Texture? = null
 
@@ -39,7 +47,12 @@ class MainScene(
         try {
             val imagePath = "images/logo.bmp".toPath()
             val image = BmpReader().read(imagePath)
-            texture = TextureLoader().load(image)
+            texture = TextureLoader().loadTexture(image)
+
+            fontAtlas = fontAtlasLoader.load(FileContentProvider("fonts/Platinum Sign Over.fnt".toPath()))
+            val fontImagePath = ("fonts/" + fontAtlas.pages.first().file).toPath()
+            val fontImage = BmpReader().read(fontImagePath)
+            fontTexture = TextureLoader().loadFontTexture(fontAtlas, fontImage)
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -60,6 +73,10 @@ class MainScene(
                     translate(size.first / 2f - it.width / 2f, size.second / 2f - it.height / 2f, 0f)
                     it.render()
                 }
+            }
+
+            pushed {
+                fontTexture.render('A', 50f)
             }
         }
     }
